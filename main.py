f6a9f76460c507b51a26d129cac0121d182cc4b1
Nuraiym077 <jenishbekovanuraiym07@gmail.com>
2025-10-03 14:14:25 +0600

    I added two python files

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-bbdhdjddjjjdjdjdjjdjklllwllwmmmc
-,d,,d,d,d,,,
-
-s';;ddddddd'
+class Person:
+    def __init__(self, name,last_name, age, city):
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+    def info(self):
+       return f' {self.name} {self.last_name} {self.age} {self.city}'
+pro1=Person('Nuraiym', 'Zhenishbekova', 18, 'Talas')
+print(pro1.info())
--- person.py
+++ person.py
@@ -1,5 +1,6 @@
-mmfmfmfnbdbdbywywwowow
-wjwjjjwebyrfbuwhggdc
-lfkkfkfkfk
-jrnrnfufru922gfbf
-nnvnnnvn
+cars= ['mers', 'bmw', 'honda', 'tesla']
+name=input('car name:')
+if name in cars:
+    print('Мындай машина бар')
+else:
+    print('Мындай машина жок')
